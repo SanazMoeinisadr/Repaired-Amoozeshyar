7fe65ebde5e0811c404b04cfcd82538fd7ddfa6d
Danial Alidaryan <alidaryandanial@gmail.com>
2024-12-02 18:03:33 +0330

    init contact us view

--- Home/urls.py
+++ Home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.homePage, name="Home")
+    path("", views.homePage, name="Home"),
+    path("contactUs", views.contactUS, name="contactUs"),
 ]
