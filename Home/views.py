dbf21e6ce255b7709ffab993f275aa25988c5e8c
Danial Alidaryan <alidaryandanial@gmail.com>
2024-12-12 12:09:09 +0330

    delete all files

--- Home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def homePage(request):
-    return render(request, 'Home/index.html', context={})
-
-def contactUS(request):
-    return render(request, 'Home/contactUs.html', context={})
-
-# Create your views here.
--- README.md
+++
@@ -1,4 +0,0 @@
-Create a better version of amoozeshyar
-to enter admin panel:
-username : danial
-pass : 1234
